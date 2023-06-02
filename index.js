104c9f9e77f2b310e1ecd476202f60d8b02e1e2b
Alesa <alesa@live.se>
2023-06-02 16:03:42 +0200

    delete

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-const button = document.createElement('button');
-button.innerHTML = 'Click me for a random number!';
+import { handleClick } from './click-btn-q'
 
-const randomNumberText = document.createElement('span');
+function displayClick() {
 
-button.addEventListener('click', function() {
-  const randomNumber = Math.floor(Math.random() * 100) + 1;
-  console.log(randomNumber);
-  randomNumberText.innerHTML =  '<br>' + randomNumber;
-});
+    const button = document.createElement('button');
+    button.innerHTML = 'Click me for a random number!';
 
-document.body.append(button, randomNumberText);
+    const randomNumberText = document.createElement('span');
+
+    button.addEventListener('click', ()  => {
+
+        // const randomNumber = Math.floor(Math.random() * 100) + 1;
+        // console.log(randomNumber);
+
+        randomNumberText.innerHTML = '<br>' + randomNumber;
+        
+    });
+
+    document.body.append(button, randomNumberText);
+}
+
+module.exports = {
+    displayClick,
+};
